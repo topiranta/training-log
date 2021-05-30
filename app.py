59684372f3bea24b9a6401b13e34c301d8d39b50
Topi Ranta <topi.ranta@gmail.com>
2021-05-30 21:43:23 +0300

    database created

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+try:
+	db.session.execute("CREATE TABLE exercises (id SERIAL PRIMARY KEY, description TEXT);")
+except:
+	print('could not create table probably because it exists already')
+
+db.session.commit()
+
+try:
+	db.session.execute("INSERT INTO exercises (description) VALUES ('kovajuoksulenkki');")
+	db.session.execute("INSERT INTO exercises (description) VALUES ('kovapyoralenkki');")
+except:
+	print('ei nyt onnistunu')
+db.session.commit()
 
 @app.route("/")
 def index():
-	return "Nyt niitä kilometrejä!"
+	
+	result = db.session.execute("SELECT description FROM exercises")
+	messages = result.fetchall()
+	return str(messages)
