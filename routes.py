daf2cb4edd682c16a3f12e58755ca573b1ca5909
Topi Ranta <topi.ranta@gmail.com>
2021-06-09 13:22:17 +0300

    Add login, logout, user error messages

--- routes.py
+++ routes.py
@@ -3,34 +3,87 @@ from flask import redirect, render_template, request, session
 from db import db
 import users
 
+
 @app.route("/")
 def index():
+    if not 'username' in session:
 
-	if not 'username' in session:
+        error = request.args.get('error')
+        loginError = ''
+        createError = ''
 
-		return render_template("login.html")
+        if error == 'login_failed':
 
-	result = db.session.execute("SELECT description FROM exercises")
-	exercises = result.fetchall()
-	return render_template("index.html", exercises=exercises)
+            loginError = 'Kirjautuminen epäonnistui: käyttäjä tai salasana on väärä'
+
+        elif error == 'unmatching_passwords':
+
+            createError = 'Syöttämäsi salasanat eivät täsmää'
+
+        elif error == 'username_not_available':
+
+            createError = 'Käyttäjätunnus ei ole vapaana'
+
+        return render_template("login.html", login_error=loginError, create_error=createError)
+
+    username = session['username']
+    result = db.session.execute("SELECT description FROM exercises")
+    exercises = result.fetchall()
+
+    return render_template("index.html", exercises=exercises, username=username)
+
 
 @app.route("/add", methods=["POST"])
 def add():
+    exercise = request.form["exercise"]
+    sql = "INSERT INTO exercises (description) VALUES (:exercise);"
 
-	exercise = request.form["exercise"]
-	sql = "INSERT INTO exercises (description) VALUES (:exercise);"
-	try:
-		db.session.execute(sql, {"exercise":exercise})
-	except:
-		return 'ei nyt onnistunu'
-	db.session.commit()
+    try:
 
-	return redirect("/")
+        db.session.execute(sql, {"exercise": exercise})
 
-@app.route("/create-user",methods=["POST"])
+    except:
+
+        return 'ei nyt onnistunu'
+
+    db.session.commit()
+    return redirect("/")
+
+
+@app.route("/create-user", methods=["POST"])
 def createUser():
-	username = request.form["username"]
-	password = request.form["password"]
-	if users.createUser(username, password):
-		return redirect("/")
-	return render_template("error.html")
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+
+    if password1 != password2:
+
+        return redirect("/?error=unmatching_passwords")
+
+    if users.create(username, password1):
+        session['username'] = username
+        return redirect("/")
+
+    return redirect("/?error=username_not_available")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    if users.login(username, password):
+        session['username'] = username
+        return redirect("/")
+
+    return redirect("/?error=login_failed")
+
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
+
+@app.route("/logout")
+def logout():
+    del session['username']
+    return redirect('/')
--- users.py
+++ users.py
@@ -3,15 +3,37 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
 
-def createUser(username, password):
+def create(username, password):
 
 	password_hash = generate_password_hash(password)
 	try:
+
 		sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
 		db.session.execute(sql, {"username":username,"password":password_hash})
 		db.session.commit()
+
 	except:
 
 		return False
 
 	return True
+
+def login(username, password):
+
+	try:
+		sql = "SELECT password FROM users WHERE username=:username"
+		user = db.session.execute(sql, {"username":username}).fetchone()
+
+		if not (user == None):
+
+
+			if check_password_hash(user[0], password):
+
+				return True
+
+		return False
+
+	except:
+
+		return False
+
